e3a3ea5fe2770d44e1027b274492bc864299a47f
Rakib Hosain <143322856+RakibHosain2302@users.noreply.github.com>
2023-11-05 10:59:32 +0600

    Add files via upload

---
+++ Js/app.js
@@ -0,0 +1,42 @@
+
+$(function(){
+
+ let hideBtn = $(".hide")
+ let showBtn = $(".show")
+ let toggleBtn = $(".toggle")
+ let box = $('.box')
+
+
+ hideBtn.click(() => {
+     box.hide(500)
+ })
+
+
+
+ showBtn.click(() => {
+    box.show(800)
+ })
+
+
+toggleBtn.click(() => {
+    box.toggle(900)
+ })
+
+let navBTn = $("nav button")
+let sidebarBtn = $(".side")
+let btn = $(".Btn")
+
+navBTn.click(() =>{
+    btn.toggleClass('active')
+    sidebarBtn.toggleClass('active')
+    navBTn.toggleClass('is-active')
+})
+
+
+
+$(window). on('load',() =>{
+    $('.preloader').fadeOut(2000)
+})
+
+
+})
